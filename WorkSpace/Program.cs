7a74800c9b73c9006456f18d103a9c8b43e2237c
Nilsu <nilsu5234@gmail.com>
2024-05-01 19:01:14 +0300

    first commit

---
+++ WorkSpace/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concreate;
+using Entities.Concreate;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.FirstName = "NİLSU";
+        person.Lastname = "ÖZKAN";
+        person.DateOfBirth = 2000;
+        person.NationalIdentity = 123;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person);
+    }
+}
+
+
